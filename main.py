99fb28f84c987fb40106de1ae51a263cf16e82f1
Arthur LASNIER <Arthur@mbparthrlasnier.home>
2024-11-07 23:18:57 +0100

    Modified the code to make it easier to read

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def facteurs_premiers(n, div=2):
+def prime_factors(n, div=2):
     # Condition d'arrêt : Si n = 1, on a fini la factorisation
     if n == 1:
 
@@ -7,9 +7,9 @@ def facteurs_premiers(n, div=2):
     # Si n est divisible par le diviseur actuel
     if n % div == 0:
 
-        return [div] + facteurs_premiers(n // div, div)
+        return [div] + prime_factors(n // div, div)
 
     # Sinon on augmente le diviseur et continue la récursion
-    return facteurs_premiers(n, div + 1)
+    return prime_factors(n, div + 1)
 
-print(facteurs_premiers(60))
+print(prime_factors(60))
